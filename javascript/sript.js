4f3d086851182b6c9f36f264c2c3fd8d3b36a8c5
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-02-13 12:18:16 +0500

    addd

--- javascript/sript.js
+++ javascript/sript.js
@@ -1,3 +1,5 @@
+
+// 1
 const rename = document.getElementById('name');
 const btn_rename = document.querySelector('.btn_rename');
 
@@ -18,4 +20,23 @@ function renameBtn () {
 
 btn_rename.addEventListener('click', renameBtn);
 
-//
+// 2
+const rename2 = document.getElementById('name2');
+const btn_rename2 = document.querySelector('.btn_rename2');
+
+rename2.addEventListener('click', function rnm2() {
+		
+	const name2 = document.querySelector('.rename2');
+
+	name2.style.display = 'block';
+});
+
+function renameBtn2 () {
+	let inp2 = document.getElementById('renameInp2').value;
+	const name2 = document.querySelector('.rename2');
+
+	rename2.innerHTML = inp2;
+	name2.style.display = 'none';
+}
+
+btn_rename2.addEventListener('click', renameBtn2);
